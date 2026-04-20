6a9970e6eff286405d433d6eac54cc4b7b03e2fe
Bar Nuri <barnu@checkpoint.com>
2026-04-20 14:42:29 +0300

    windock 1.0.37

--- Formula/windock.rb
+++ Formula/windock.rb
@@ -1,9 +1,9 @@
 class Windock < Formula
   desc "Windows 11-style taskbar for macOS"
   homepage "https://github.com/barnuri/win-dock"
-  url "https://github.com/barnuri/win-dock/releases/download/v1.0.36/WinDock.zip"
-  version "1.0.36"
-  sha256 "e4b9eadb2048b6f0069c2e974240dc3985a74a88c14437afa9c640ee1cfdb961"
+  url "https://github.com/barnuri/win-dock/releases/download/v1.0.37/WinDock.zip"
+  version "1.0.37"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   depends_on macos: :sonoma
 
